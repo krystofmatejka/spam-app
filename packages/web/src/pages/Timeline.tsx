1f769da51f1bc1658e813aa6165c69d13e3f3fdd
Kryštof Matějka <kx.matejka@gmail.com>
2019-06-16 19:17:23 +0200

    feat: add first version of add post form

--- packages/web/src/pages/Timeline.tsx
+++ packages/web/src/pages/Timeline.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import Link from 'next/link'
+import { Mutation, ApolloContext } from 'react-apollo'
 import gql from 'graphql-tag'
 import { graphqlClient } from '../lib/graphqlClient'
 import { ROUTES } from '../constants'
@@ -7,6 +8,15 @@ import { ROUTES } from '../constants'
 const QUERY_GET_POSTS = gql`
   query {
     getPosts {
+      id
+      text
+    }
+  }
+`
+
+const MUTATION_CREATE_POST = gql`
+  mutation ($input: PostInput!)  {
+    createPost (input: $input) {
       id
       text
     }
@@ -22,19 +32,80 @@ interface Props {
   posts: Post[]
 }
 
-const Timeline = ({ posts }: Props) => {
-  return posts.map(post => (
-    <h2>
-      <Link
-        href={{ pathname: ROUTES.POST.page, query: { id: post.id } }}
-        as={`/post/${post.id}`}
-      >
-        <a>
-          {post.text}
-        </a>
-      </Link>
-    </h2>
-  ))
+const AddPostForm = ({ setPosts }) => {
+  const [post, setPost] = useState('')
+
+  return (
+    <Mutation
+      mutation={MUTATION_CREATE_POST}
+      update={(cache, { data }) => {
+        const posts = cache.readQuery({
+          query: QUERY_GET_POSTS
+        })
+        cache.writeQuery({
+          query: QUERY_GET_POSTS,
+          data: {
+            getPosts: posts.getPosts.concat([ data.createPost ])
+          }
+        })
+        setPosts(posts.getPosts.concat([ data.createPost ]))
+      }}
+    >
+      {( createPost: Function ) => (
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          //console.log(post)
+          createPost({
+            variables: {
+              input: {
+                text: post
+              }
+            }
+          })
+        }}>
+          <label>
+            Post:
+            <textarea onChange={(e) => setPost(e.target.value)} value={post} />
+          </label>
+          <input type='submit' value='Submit'/>
+        </form>
+      )}
+    </Mutation>
+  )
+}
+
+const Timeline = ({ posts: inputPosts }: Props) => {
+  const [ posts, setPosts ] = useState(inputPosts)
+  const { client } = useContext(ApolloContext)
+
+  useEffect(() => {
+    client.writeQuery({
+      query: QUERY_GET_POSTS,
+      data: {
+        getPosts: posts
+      }
+    })
+  }, [])
+
+  return (
+    <>
+      <AddPostForm setPosts={setPosts}/>
+      {
+        posts.map(post => (
+          <h2>
+            <Link
+              href={{pathname: ROUTES.POST.page, query: {id: post.id}}}
+              as={`/post/${post.id}`}
+            >
+              <a>
+                {post.text}
+              </a>
+            </Link>
+          </h2>
+        ))
+      }
+    </>
+  )
 }
 
 Timeline.getInitialProps = async () => {
